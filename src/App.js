2d44bea834accd729194a12a0a5da3d498b90694
DanielCuevas1822 <daniel_cuevas_hernandez@outlook.com>
2021-04-20 16:47:06 -0500

    Add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { Route, Switch } from "react-router";
+import Footer from "./components/Footer";
+
+import Header from "./components/Header";
+import Anuncios from "./views/Anuncios/Anuncios";
+import SingleAnuncios from "./views/Anuncios/SingleAnuncios";
+import Blog from "./views/Blog/Blog";
+import SingleBlog from "./views/Blog/SingleBlog";
+import Contacto from "./views/Contacto";
+import Home from "./views/Home";
+import Nosotros from "./views/Nosotros";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/nosotros" component={Nosotros} />
+        <Route exact path="/anuncios" component={Anuncios} />
+        <Route exact path="/anuncios/:name" component={SingleAnuncios} />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/blog/:name" component={SingleBlog} />
+        <Route exact path="/contacto" component={Contacto} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
+import "./assets/style/normalize.css";
+import "./assets/style/main.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
